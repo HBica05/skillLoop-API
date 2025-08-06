71e4bf6fe2f26dd2837f185b0ffbdf9b8eab5efd
HBica05 <bicahaadiyah@gmail.com>
2025-08-06 18:05:04 +0200

    Restore ProfileSerializer and complete custom RegisterSerializer for user signup

--- users/serializers.py
+++ users/serializers.py
@@ -1,17 +1,18 @@
-from rest_framework import serializers
-from .models import Profile
-from django.contrib.auth.models import User
 from dj_rest_auth.registration.serializers import RegisterSerializer as DefaultRegisterSerializer
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile
+
 
 class RegisterSerializer(DefaultRegisterSerializer):
     bio = serializers.CharField(required=False)
     location = serializers.CharField(required=False)
 
     def get_cleaned_data(self):
-        cleaned_data = super().get_cleaned_data()
-        cleaned_data['bio'] = self.validated_data.get('bio', '')
-        cleaned_data['location'] = self.validated_data.get('location', '')
-        return cleaned_data
+        data = super().get_cleaned_data()
+        data['bio'] = self.validated_data.get('bio', '')
+        data['location'] = self.validated_data.get('location', '')
+        return data
 
     def custom_signup(self, request, user):
         Profile.objects.create(
@@ -19,6 +20,7 @@ class RegisterSerializer(DefaultRegisterSerializer):
             bio=self.validated_data.get('bio', ''),
             location=self.validated_data.get('location', '')
         )
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
