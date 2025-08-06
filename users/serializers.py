92302931d5f45ffee4ce00562b180551ea8d75de
HBica05 <bicahaadiyah@gmail.com>
2025-08-06 09:51:06 +0200

    Add custom RegisterSerializer to include bio and location fields

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,17 @@
-from dj_rest_auth.registration.serializers import RegisterSerializer as DefaultRegisterSerializer
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib.auth.models import User
+from dj_rest_auth.registration.serializers import RegisterSerializer as DefaultRegisterSerializer
 
-# Custom registration serializer to include extra fields (optional)
 class RegisterSerializer(DefaultRegisterSerializer):
     bio = serializers.CharField(required=False)
     location = serializers.CharField(required=False)
+
+    def get_cleaned_data(self):
+        cleaned_data = super().get_cleaned_data()
+        cleaned_data['bio'] = self.validated_data.get('bio', '')
+        cleaned_data['location'] = self.validated_data.get('location', '')
+        return cleaned_data
 
     def custom_signup(self, request, user):
         Profile.objects.create(
@@ -15,7 +20,6 @@ class RegisterSerializer(DefaultRegisterSerializer):
             location=self.validated_data.get('location', '')
         )
 
-# Serializer for Profile CRUD functionality
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
 
